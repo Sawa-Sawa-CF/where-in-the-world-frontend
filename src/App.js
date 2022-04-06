af45cb8abbb6d11dcfa01693742d7ed06a6a3caa
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-04-05 17:08:41 -0700

    stuff we did

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import React from 'react';
 import LogoutButton from './LogoutButton';
 import LoginButton from './LoginButton';
 import Content from './Content.js';
+import Profile from './Profile';
+import Footer from './Footer';
 import './App.css';
 import './Header.css';
 import Header from './Header'
 import { withAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter as Router,  Routes, Route } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-       user: null
+      user: null
     }
   }
 
-  
+
 
 
   render() {
     console.log(this.props.auth0.isAuthenticated);
     return (
-    
-      
+
+
       <>
-      <Header/>
         {
           this.props.auth0.isAuthenticated
             ? <LogoutButton />
@@ -33,12 +35,30 @@ class App extends React.Component {
         }
         {
           this.props.auth0.isAuthenticated
-            ? <Content/>
+            ?
+            <Router>
+              <Header user={this.state.user} onLogout={this.logoutHandler} />
+              <Routes>
+                <Route exact path="/">
+                  {this.props.auth0.isAuthenticated ? <Content user={this.state.user} /> : <h2>Please Log In</h2>}
+                </Route>
+                <Route exact path="/Profile">
+                  <Profile userInfo={this.state.user} />
+                </Route>
+                <Route exact path="/LoginButton">
+                  {/* <LoginForm loginHandler={this.setLogin} /> */}
+                </Route>
+                <Route exact path="/LogoutButton">
+                  <LogoutButton onLogout={this.logoutHandler} />
+                </Route>
+              </Routes>
+              <Footer />
+            </Router>
             : <h2>Please Log In to </h2>
         }
       </>
-     
-      
+
+
     )
   }
 }
--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import Profile from './Profile';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
+// import Profile from './Profile';
+// import LoginButton from './LoginButton';
+// import LogoutButton from './LogoutButton';
 import { withAuth0 } from "@auth0/auth0-react";
 import YelpResult from './YelpResult';
 
@@ -10,35 +10,46 @@ class Content extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            yelpData: [],
+            yelpData: []
         }
     }
 
-    getYelpResults = async () => {
-        // JSON Web Token = JWT (pronounced JOT)
-        if (this.props.auth0.isAuthenticated) {
+    getRestaurants = async () => {
+        // // JSON Web Token = JWT (pronounced JOT)
+        // if (this.props.auth0.isAuthenticated) {
             
-            // get token:
-            const res = await this.props.auth0.getIdTokenClaims();
+        //     // get token:
+        //     const res = await this.props.auth0.getIdTokenClaims();
             
-            // MUST use double underscores
-            const jwt = res.__raw;
-            // a console.log of the token // this is as far as you need to go for the lab. Get the jwt to log to the console.
+        //     // MUST use double underscores
+        //     const jwt = res.__raw;
+        //     // a console.log of the token // this is as far as you need to go for the lab. Get the jwt to log to the console.
 
-            // as per axios docs, we can send an config object to make our call as well
-            const config = {
-                method: 'get',
-                baseURL: process.env.REACT_APP_SERVER,
-                url: '/restaurants',
-                headers: { "Authorization": `Bearer ${jwt}` }
-            };
-            const queryResults = await axios(config);
-            console.log(queryResults.data.businesses);
-            this.state.yelpData = queryResults.data.businesses;
+        //     // as per axios docs, we can send an config object to make our call as well
+        //     const config = {
+        //         method: 'get',
+        //         baseURL: process.env.REACT_APP_SERVER,
+        //         url: '/restaurants',
+        //         headers: { "Authorization": `Bearer ${jwt}` }
+        //     };
+        //     const queryResults = await axios(config);
+        //     console.log(queryResults.data.businesses);
+        //     this.state.yelpData = queryResults.data.businesses;
+        // }
+
+        try{
+            let restaurants = await axios.get(`${process.env.REACT_APP_SERVER}/restaurants`);
+            this.setState({
+                yelpData: restaurants.data
+            })
+            console.log(this.state.yelpData);
+        }catch(error){
+            console.log(`error message `, error);
         }
     }
+
     componentDidMount() {
-        this.getYelpResults();
+        this.getRestaurants();
     }
 
     render() {
