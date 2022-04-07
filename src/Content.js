3dfd21f251b65ef00dc2f1b9712fb252da02b888
Abdinasir Yussuf <76438529+abditake@users.noreply.github.com>
2022-04-07 14:21:34 -0700

    Revert "Katrina day4c"

--- src/Content.js
+++ src/Content.js
@@ -1,77 +1,129 @@
 import React from 'react';
+// import axios from 'axios';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Content.css';
 
+// import Profile from './Profile';
+// import LoginButton from './LoginButton';
+// import LogoutButton from './LogoutButton';
+// import { withAuth0 } from "@auth0/auth0-react";
+// import YelpResult from './YelpResult';
+
 class Content extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    constructor(props) {
+        super(props);
+         this.state = {
+      
+         }
+
     }
-  }
 
-  performSearch = (e) => {
-    e.preventDefault();
-    const city = e.target.City.value;
-    const food = e.target.Food.value;
-    console.log(city);
-    console.log(food);
-    this.props.getRestaurants(city, food);
-  }
+    // getRestaurants = async (city, food) => {
+    //     try {
+    //         let restaurants = await axios.get(`${process.env.REACT_APP_SERVER}/restaurants?location=${city}&term=${food}`);
+    //         console.log(restaurants);
+    //         this.setState({
+    //             yelpData: restaurants.data
+    //         })
+    //     } catch (error) {
+    //         console.log(`error message `, error);
+    //     }
+    // }
 
-  handleAddButton = (restaurant) => {
-    // this.state.yelpDataForProfile.push(restaurant);
-    console.log('updated state after add button pressed: ', this.props.yelpDataForProfile);
-    console.log('restaurant passed in to button: ', restaurant);
-    this.props.postRestaurants(restaurant);
-  }
 
-  render() {
-    console.log('updated state after add button pressed: ', this.props.yelpDataForProfile);
-    return (
-      <>
-        <section className='form-section'>
-          <Form onSubmit={this.performSearch}>
-            <Form.Group controlId="City">
-              <Form.Label>City</Form.Label>
-              <Form.Control type="text" />
-            </Form.Group>
-            <Form.Group controlId="Food">
-              <Form.Label>Food</Form.Label>
-              <Form.Control type="text" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Search
-            </Button>
-          </Form>
-        </section>
-        <section className='card-container'>
-          {
-            this.props.yelpData
-              ?
-              this.props.yelpData.map((restaurantData, idx) =>
-                <Card className='restaurants' style={{ width: '18rem' }} key={this.props.yelpData.indexOf(restaurantData)}>
-                  <Card.Img className='img' variant="top"
-                    src={restaurantData.image_url} />
-                  <Card.Body>
-                    <Button className='button' variant="primary" onClick={() => this.handleAddButton(restaurantData)}>Add</Button>
-                    <Card.Title>{restaurantData.name} </Card.Title>
-                    <Card.Subtitle>{restaurantData.rating} Stars⭐</Card.Subtitle>
-                    <Card.Text>
-                      {restaurantData.address1}, {restaurantData.city}, {restaurantData.state} {restaurantData.zip_code}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              )
-              :
-              <></>
-          }
-        </section>
-      </>
-    );
-  }
+    performSearch = (e) => {
+        e.preventDefault();
+        const city = e.target.City.value;
+        const food = e.target.Food.value;
+
+        console.log(city);
+        console.log(food);
+        this.props.getRestaurants(city, food);
+    }
+
+
+    handleAddButton = (restaurant) => {
+        // this.state.yelpDataForProfile.push(restaurant);
+        console.log('updated state after add button pressed: ', this.props.yelpDataForProfile);
+        console.log('restaurant passed in to button: ', restaurant);
+        this.props.postRestaurants(restaurant);
+
+    }
+
+
+    //     postRestaurants = async (newRestaurant) => {
+    //         try {
+    //             let dataToPost = await axios.post(`${process.env.REACT_APP_SERVER}/restaurants`, newRestaurant);
+    //             console.log(dataToPost.data, 'dataToPost'); 
+    //             this.setState({
+    //             yelpDataForProfile: [...this.state.yelpDataForProfile, dataToPost.data]
+    //         })
+    //         console.log(`${newRestaurant.name} added to yelpDataForProfile`);
+    //     } catch(error) {
+    //         console.log('we have an error: ', error.response.data)
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     this.getRestaurants();
+    // }
+
+    render() {
+        console.log('updated state after add button pressed: ', this.props.yelpDataForProfile);
+        return (
+            <>
+                <section className='form-section'>
+                    <Form onSubmit={this.performSearch}>
+                        <Form.Group controlId="City">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" />
+                        </Form.Group>
+                        <Form.Group controlId="Food">
+                            <Form.Label>Food</Form.Label>
+                            <Form.Control type="text" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Search
+                        </Button>
+                    </Form>
+                </section>
+
+
+
+
+            <section className='card-container'>
+                {
+                    this.props.yelpData
+                        ?
+
+                        this.props.yelpData.map((restaurantData, idx) =>
+                        <Card className='restaurants'style={{ width: '18rem' }} key={this.props.yelpData.indexOf(restaurantData)}>
+
+                                <Card.Img className='img' variant="top"
+                                    src={restaurantData.image_url} />
+                                <Card.Body>
+                            <Button className='button' variant="primary" onClick={() => this.handleAddButton(restaurantData)}>Add</Button>
+                                    <Card.Title>{restaurantData.name} </Card.Title>
+                                    <Card.Subtitle>{restaurantData.rating} Stars⭐</Card.Subtitle>
+                                    <Card.Text>
+                                        {restaurantData.address1}, {restaurantData.city}, {restaurantData.state} {restaurantData.zip_code}
+                                    </Card.Text>
+
+                                </Card.Body>
+                            </Card>
+
+                        )
+
+                        :
+                        <></>
+                }
+            </section>
+            </>
+        );
+    }
 }
 
 export default Content;
--- src/Footer.js
+++ src/Footer.js
@@ -3,25 +3,38 @@ import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
+
+
+
 export default class Footer extends Component {
   render() {
+
+
+
+
     return (
       <>
         <footer class="footer-distributed">
+
           <div class="footer-right">
+
             <a href="#"><FontAwesomeIcon icon="fa-brands fa-github" /></a>
             <a href="#"><FontAwesomeIcon icon="fa-brands fa-twitter" /></a>
             <a href="#"><FontAwesomeIcon icon="fa-brands fa-linkedin" /></a>
           </div>
+
           <div class="footer-left">
+
             <p class="footer-links">
               <a class="link-1" href="#">Home</a>
               <a href="#">About</a>
             </p>
             <p>Code Fellows &copy; 2022</p>
           </div>
+
         </footer>
       </>
+
     )
   }
 }
