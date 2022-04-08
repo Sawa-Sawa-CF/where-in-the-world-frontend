be73a8cc47dbaa4736048ba7783ee38432911714
Katrina Lenell Hill <khill365@gmail.com>
2022-04-07 17:51:14 -0700

    removed extra spaces

--- src/Profile.js
+++ src/Profile.js
@@ -13,25 +13,14 @@ class Profile extends React.Component {
       places: [],
       placeToUpdate: {},
       show: false,
-
     }
-
   }
 
   handleClose = () => this.setState({ show: false });
 
-  // handleChange = (e) => {
-  //   let currentPlace = 
-  //   this.setState
-  // ({ notes: e.target.value })
-  // }
-
-
-
   getSavedPlaces = async () => {
     try {
       let savedPlaces = await axios.get(`${process.env.REACT_APP_SERVER}/savedRestaurants`);
-
       this.setState({
         places: savedPlaces.data
       })
@@ -39,7 +28,6 @@ class Profile extends React.Component {
       console.log(`error message `, error);
     }
   }
-
 
   componentDidMount() {
     this.getSavedPlaces();
@@ -57,40 +45,24 @@ class Profile extends React.Component {
 
   changeShowModal = (place) => {
     console.log(place._id);
-    // for (let i = 0; i < this.state.places.length; i++) {
-    //   let currentPlace = this.state.places[i];
-    //   if (currentPlace._id === place._id) {
-
-    //   }
-    // }
     console.log(place);
     this.setState({
       show: true,
       placeToUpdate: place,
-
     });
-
   }
 
   notesUpdated = (updatedNotes) => {
     let currentNotes = this.state.place.notes;
     currentNotes = updatedNotes;
     this.setState({ place: currentNotes });
-
   }
 
   handleUpdate = (newNotes) => {
     this.handleSubmit();
     let updatedPlace = this.state.placeToUpdate;
-    console.log(updatedPlace, 'updatewdpalce');
     updatedPlace.notes = newNotes;
-    // this.setState({
-    //   placeToUpdate: updatedPlace,
-    // })
-    console.log(updatedPlace);
     this.updateSavedPlace(updatedPlace);
-
-
   };
 
   handleSubmit = async (e) => {
@@ -101,55 +73,15 @@ class Profile extends React.Component {
      show: false,
    })
    let response = await axios.put(`${process.env.REACT_APP_SERVER}/restaurants/${this.state.placeToUpdate._id}`, this.state.placeToUpdate);
-   console.log(response.data);
    this.getSavedPlaces();
-   
   }
-
-
-
-
-
-
-
-
-
-
-
 
   render() {
     return (
       <>
-
-
-        {/* 
-        <div className='card-container' >
-
-          {this.state.places && this.state.places.map((restaurant, idx) => {
-            return (
-              <Card className="restaurants" style={{ width: '18rem' }} key={idx}>
-
-                <Card.Img className='img' variant="top"
-                  src={restaurant.image_url} />
-                <Card.Body>
-                  <Card.Title>{restaurant.name} </Card.Title>
-                  <Card.Subtitle>{restaurant.rating}Stars</Card.Subtitle>
-                  <Card.Text>
-                    {restaurant.address1}, {restaurant.city}, {restaurant.state} {restaurant.zip_code}
-                  </Card.Text>
-                  <Button className='button' variant="primary" onClick={() => this.deleteSavedPlace(restaurant)} >Delete</Button>
-                  <Button className='button' variant="primary" onClick={() => this.changeShowModal(restaurant)} >Edit</Button>
-                </Card.Body>
-              </Card>
-
-            )
-          })}
-        </div> */}
-
         {
           this.state.show ?
             (
-
               <Modal
                 show={this.state.show} onHide={this.handleClose}>
                 <Modal.Header closeButton>
@@ -162,24 +94,18 @@ class Profile extends React.Component {
                       Type your notes here
                     </Form.Label>
                     <Form.Control id='FormText' type="text" >
-
                     </Form.Control>
                     <Button className='button' variant="primary" type="submit"  >Edit</Button>
-                    {/* onClick={() => this.handleUpdate(this.state.placeToUpdate.notes)} */}
                   </Form.Group>
                   </Form>
                 </Modal.Body>
-
               </Modal>
-
             ) :
             (
               <div className='card-container' >
-
                 {this.state.places && this.state.places.map((restaurant, idx) => {
                   return (
                     <Card className="restaurants" style={{ width: '18rem' }} key={idx}>
-
                       <Card.Img className='img' variant="top"
                         src={restaurant.image_url} />
                       <Card.Body>
@@ -189,25 +115,18 @@ class Profile extends React.Component {
                           {restaurant.address1}, {restaurant.city}, {restaurant.state} {restaurant.zip_code} 
                           <br></br>
                           Notes: {restaurant.notes}
-                          
                         </Card.Text>
                         <Button className='button' variant="primary" onClick={() => this.deleteSavedPlace(restaurant)} >Delete</Button>
                         <Button className='button' variant="primary" onClick={() => this.changeShowModal(restaurant)} >Edit</Button>
                       </Card.Body>
                     </Card>
-
                   )
                 })}
               </div>
             )
         }
-
-
-
       </>)
-
   }
 }
-
 
 export default Profile;
